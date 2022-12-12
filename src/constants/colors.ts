befc4b79b206cdc0b1be0afbbb77f0e96f90060e
iremlaya <iremlcaya@gmail.com>
2022-12-12 04:08:39 +0300

    feat: interface and types defined

---
+++ src/constants/colors.ts
@@ -0,0 +1,10 @@
+// todo :: find a better way to store these colors, since it needs to be updated in tailwind config as well
+export const backgroundColors = [
+  "bg-pink",
+  "bg-coral",
+  "bg-lemon",
+  "bg-green",
+  "bg-lightBlue",
+  "bg-blue",
+  "bg-yellow",
+];
